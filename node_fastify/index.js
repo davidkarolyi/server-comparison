3c82b7108f9cb7917b21b552827eaf48c6072324
David Karolyi <davidkarolyi96@gmail.com>
2020-11-09 22:16:05 +0100

    feat: Add node_fastify server implementation

---
+++ node_fastify/index.js
@@ -0,0 +1,29 @@
+const fastify = require("fastify")();
+const jwt = require("jsonwebtoken");
+const fs = require("fs");
+const path = require("path");
+const { secret } = require("../test_data.json");
+
+fastify.get("/", async (request, reply) => {
+  const token = await readToken();
+  const payload = verifyToken(token);
+  return payload;
+});
+
+const start = async () => {
+  await fastify.listen(3000);
+  console.log(`node_fastify is listening on localhost:3000`);
+};
+start();
+
+async function readToken() {
+  const tokenString = await fs.promises.readFile(
+    path.resolve(__dirname, "../test_data.json"),
+    { encoding: "utf8" }
+  );
+  return JSON.parse(tokenString).token;
+}
+
+function verifyToken(token) {
+  return jwt.verify(token, secret);
+}
